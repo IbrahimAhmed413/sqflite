37e333795f3ab9b64dc780197ae4dd2aa2d15929
Ibrahim's <ibrahimahmed12333@gmail.com>
2024-03-14 11:24:17 +0500

    made a homescreen and run app screen, home screen is yet having app bar and a tittle which I'm accessing from main.dart

---
+++ lib/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.withOpacity(0.5),
+        title: Text("To-do SQFlite"),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/cupertino.dart';
+import 'package:sqflite/app.dart';
+
+void main() => runApp(App());
